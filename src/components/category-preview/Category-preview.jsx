80f3d037dc2c3e368c5545c592b4a0af20b19089
Dami K <dmk9104@gmail.com>
2024-09-17 12:47:56 +0900

    UI improved.

--- src/components/category-preview/Category-preview.jsx
+++ src/components/category-preview/Category-preview.jsx
@@ -1,21 +1,24 @@
 import { Link } from 'react-router-dom'
 import ProductCard from "../product-card/product-card.component"
-import './category-preview.styles.scss'
+// import './category-preview.styles.scss'
 
 const CategoryPreview = ({ title, products }) => {
     return(
-        <div className="category-preview-container">
-            <h2>
+          <div className="mx-auto max-w-2xl px-4 py-8 sm:px-6 sm:py-22 lg:max-w-7xl lg:px-8">
+            <h2 className="text-2xl underline font-bold tracking-tight text-gray-900">
                 <Link to={title} className="title">
                 {title.toUpperCase()}
                 </Link>
             </h2>
-            <div className="preview">
+
+
+            <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                 {
                   products.filter((_, index) => (index < 4))
                   .map((product) => <ProductCard key={product.id} product={product} />)
                 }
             </div>
+
         </div>
     )
 }
--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -9,16 +9,25 @@ const ProductCard = ({ product }) => {
     const addProductToCart = () => addItemToCart(product)
 
     return (
-        <div className="product-card-container">
-            <img src={imageUrl} alt={`${name}`}/>
-            <div className="footer">
-                <span className='name'>{name}</span>
-                <span className="price">{price}</span>
+        <div className="group relative">
+            <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+              <img src={imageUrl} alt={`${name}`} className="h-full w-full object-cover object-center lg:h-full lg:w-full"/>
             </div>
-            <Button 
-                buttonType={BUTTON_TYPE_CLASSES.inverted}
-                onClick={addProductToCart}
-                >Add to cart</Button>
+
+            <div className="mt-4 flex justify-between">
+               <div>
+                    <p className="text-sm font-medium text-gray-900">${price}</p>
+                    <h3 className="text-sm text-gray-700">
+                    <span className="inset-0">{name}</span>
+                  </h3>
+                </div>
+                <Button 
+                    className="absolute inset-x-0 bottom-14 invisible group-hover:visible"
+                    buttonType={BUTTON_TYPE_CLASSES.inverted}
+                    onClick={addProductToCart}
+                    >Add to cart
+                </Button>
+            </div>
         </div>
     )
 }
